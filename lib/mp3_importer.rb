8ce6c828d7a1d8d524975576d3b71c1816798c84
Ni Chia <nichia@gmail.com>
2017-11-13 17:34:11 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -18,13 +18,4 @@ class MP3Importer
   def import
     self.files.each { |filename| Song.new_by_filename(filename)}
   end
-
-  def import
-      self.files.each do |filename|
-          Song.new_by_filename(filename)
-      end
-  end
-
-
-
 end
