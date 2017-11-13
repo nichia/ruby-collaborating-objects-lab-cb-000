3c74beb5fd1fcdc3e4651f13beb415c1ab79dc14
Ni Chia <nichia@gmail.com>
2017-11-13 17:12:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,10 @@ class Song
     @name = title
   end
 
-  def self.new_by_filename(file_name)
+  def self.new_by_filename(filename)
+    data = filename.split(" - ")
+    song = self.new(data[1])
+    song.artist = data[0]
       song = self.new
       song.title = filename.split(" - ")[1]
       song
